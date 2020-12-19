17ee061bccb8083ed85606d146c5e9c7b351b23a
Shivom <32161179+ulegend@users.noreply.github.com>
2020-12-19 23:42:46 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -42,17 +42,6 @@ class Binary_to_text:
 				if int(j, 2) == 96:	# First occurance of '`' stops the iteration of the data
 					break
 
-		'''for j in self.data:
-			#print(j)
-			try:
-				self.string += chr(int(j, 2))
-			except:
-				self.string += chr(0)
-			if j != '' and int(j, 2) == 96:	# First occurance of '`' stops the iteration of the data
-				break
-			else:
-				pass'''
-
 		self.string.rstrip('`')
 			
 		return self.string
